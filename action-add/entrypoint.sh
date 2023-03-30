dba0532550c75a85011f54fbdd984f3f40769028
Koen Metsu <koen@kometsoft.be>
2023-03-30 09:36:43 +0200

    feat: OR-1620 use local action, remove non-asso settings

--- action-add/entrypoint.sh
+++ action-add/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-python3 /main.py -c "${1}" -e "${2}" -p "${3}" --access-sync "${4}" --access-association-registry "${5}" --env-tst "${6}" --env-stg "${7}" --env-prd "${8}" --aws-tst-access-key-id "${9}" --aws-tst-secret-access-key "${10}" --aws-tst-region-name "${11}" --aws-stg-access-key-id "${12}" --aws-stg-secret-access-key "${13}" --aws-stg-region-name "${14}" --aws-prd-access-key-id "${15}" --aws-prd-secret-access-key "${16}"  --aws-prd-region-name "${17}" --revoke-access "${18}" --access-tickets "${19}";
+python3 /main.py -c "${1}" -e "${2}" -p "${3}" --env-tst "${4}" --env-stg "${5}" --env-prd "${6}" --aws-tst-access-key-id "${7}" --aws-tst-secret-access-key "${8}" --aws-tst-region-name "${9}" --aws-stg-access-key-id "${10}" --aws-stg-secret-access-key "${11}" --aws-stg-region-name "${12}" --aws-prd-access-key-id "${13}" --aws-prd-secret-access-key "${14}"  --aws-prd-region-name "${15}" --revoke-access "${16}";
--- action-add/main.py
+++ action-add/main.py
@@ -7,11 +7,8 @@ parser = argparse.ArgumentParser(description='add new client apikey')
 
 parser.add_argument('-c','--client', help='client name', required=True)
 parser.add_argument('-e','--email', help='email', required=True)
-parser.add_argument('-p','--plan', help='plan options are ["standard"]', required=True, default="standard")
+parser.add_argument('-p','--plan', help='plan options are ["unlimited"]', required=True, default="unlimited")
 parser.add_argument('--revoke-access', help='["true", "false"] default: false', required=False, default='false')
-
-parser.add_argument('--access-sync', help='["true", "false"] default: false', required=False, default='false')
-parser.add_argument('--access-association-registry', help='["true", "false"] default: false', required=False, default='false')
 
 parser.add_argument('--env-tst', help='["true", "false"] default: false', required=False, default='false')
 parser.add_argument('--env-stg', help='["true", "false"] default: false', required=False, default='false')
@@ -29,15 +26,13 @@ parser.add_argument('--aws-prd-access-key-id', required=True)
 parser.add_argument('--aws-prd-secret-access-key', required=True)
 parser.add_argument('--aws-prd-region-name', required=False, default='eu-west-1')
 
-parser.add_argument('--access-tickets', help='["true", "false"] default: true', required=False, default='true')
-
 args = parser.parse_args()
 
 usage_plan_ids = {
-    "standard": {
-        "tst":"yhlc48",
-        "stg":"oh8xgc",
-        "prd":"m8vqe3",
+    "unlimited": {
+        "tst":"v1a1xx",
+        "stg":"",
+        "prd":"",
     }
 }
 
@@ -52,14 +47,11 @@ def get_db_table(session):
 def get_client_api_key(apikey, env):
     client_api_key = {
             "ApiKey": apikey,
-            "SyncAccess": args.access_sync == 'true',
             "UsagePlanID": usage_plan_ids[args.plan][env],
             "Description": args.email,
-            "WrAccess": args.access_association_registry == 'true',
             "Revoked": args.revoke_access == 'true',
             "ClientName": args.client,
             "Plan": args.plan,
-            "Tickets": args.access_tickets == 'true',
     }
     return client_api_key
 
