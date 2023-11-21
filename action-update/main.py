3cb06a0a1613ed849d3ec36c1fd2b32411ee1b0e
Jan Lesage <jan.lesage@vlaanderen.be>
2023-11-21 10:54:10 +0100

    chore: updated dynamodb table names for new environment

--- action-update/main.py
+++ action-update/main.py
@@ -46,7 +46,7 @@ def start_session(aws_access_key_id, aws_secret_access_key, region_name):
     return boto3.Session(aws_access_key_id, aws_secret_access_key, region_name=region_name)
 
 def get_db_table(session):
-    return session.resource('dynamodb').Table('basisregisters-association-api-gate-keys')
+    return session.resource('dynamodb').Table('verenigingsregister-api-keys')
 
 def get_client_attribute_updates(env):
     return {
